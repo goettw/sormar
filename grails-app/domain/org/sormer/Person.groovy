37b0519ad2ab95cc13991e537e2035862cf69a6a
Oliver Wolf <owolf@anynines.com>
2014-03-18 11:35:38 +0100

    Remove an has many relation from the person model since the app was not starting

--- grails-app/domain/org/sormer/Person.groovy
+++ grails-app/domain/org/sormer/Person.groovy
@@ -4,7 +4,7 @@ class Person {
     String username
     String firstName
     String lastName
-    static hasMany = [managesProducts:Product , managesReleases:Release]
+    //static hasMany = [managesProducts:Product , managesReleases:Release]
     static mappedBy = [managesProducts : 'productManager', managesReleases : 'releaseManager']
     static constraints = {
 	username unique:true 
