a8949a2a476a62ebb21a5e422256efda83338f03
goettw <wolfgang.goette@googlemail.com>
2014-04-26 21:19:00 +0200

    security beta

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -157,7 +157,7 @@ grails {
       host = "smtp.gmail.com"
       port = 465
       username = "wolfgang.goette@gmail.com"
-      password = "vuihgwdn!1!2"
+      password = ""
       props = ["mail.smtp.auth":"true",
 	       "mail.smtp.socketFactory.port":"465",
 	       "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
